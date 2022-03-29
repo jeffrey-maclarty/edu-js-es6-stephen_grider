16750258ceda611f37fa6a431e1a49ea4070ea31
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-29 09:54:39 -0400

    Completed section 12

--- section-04-the-filter-helper/main.js
+++ section-04-the-filter-helper/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+var products = [
+    {name: 'cucumber', type: 'vegetable'},
+    {name: 'banana', type: 'fruit'},
+    {name: 'celery', type: 'vegetable'},
+    {name: 'orange', type: 'fruit'},
+]
+
+var filteredProducts = [];
+
+// for (var i = 0; i < products.length; i++) {
+//     if (products[i].type === 'fruit') {
+//         filteredProducts.push(products[i]);
+//     }
+// }
+// console.log(filteredProducts);
+
+// DO NOT FORGET RETURN, YOU WILL END UP WITH AN EMPTY ARRAY
+filteredProducts = products.filter(function(product) {
+    return product.type === 'fruit';
+});
