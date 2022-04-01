346735c9d7ab87879b48cbbd0464cbcd0d9b4313
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-04-01 05:47:57 -0400

    Completed section 9

--- section-09-template-strings/main.js
+++ section-09-template-strings/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+function getMessage () {
+    const year = new Date().getFullYear();
+
+    // return "The year is " + year;
+    // return `The year is ${year}`;
+    return `The year is ${new Date().getFullYear() - 2000}`;
+}
+
+console.log(getMessage());
+
+
+// Exercise 19
+function doubleMessage(number) {
+  return `Your number doubled is ${(2 * number)}`;
+}
+
+// Exercise 20
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
